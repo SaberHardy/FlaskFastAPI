8f189429c72eb0b5a491dd1b5de43074ee7bd8c8
Consultant <samirsamado5@gmail.com>
2023-09-28 08:43:40 -0400

    upload the csv as api

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 
-@app.route('/')
-def index():
-    return render_template('upload.html')
+# @app.route('/')
+# def index():
+#     return render_template('upload.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files['file']
-    if file and file.filename.endswith('.csv'):
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return redirect(url_for('display_api', filename=file.filename))
-    else:
-        return "Please upload a valid CSV file."
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     file = request.files['file']
+#     if file and file.filename.endswith('.csv'):
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+#         return redirect(url_for('display_api', filename=file.filename))
+#     else:
+#         return "Please upload a valid CSV file."
 
 
-@app.route('/display_api/<filename>')
-def display_api(filename):
+@app.route('/', methods=['POST', 'GET'])
+def display_api():
     api_data = []
-    with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'r') as csvfile:
+    with open("staticfiles/uploads/homes.csv", 'r') as csvfile:
+        # next(csv)
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
             api_data.append(dict(row))
