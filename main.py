588433845461779a20deebf3545caa549eb355b8
Consultant <samirsamado5@gmail.com>
2023-10-04 20:42:13 -0400

    life api from csv file

--- main.py
+++ main.py
@@ -1,40 +1,92 @@
 import os
 import csv
-from flask import Flask, render_template, request, redirect, url_for, jsonify, json
+import json
+from flask import Flask, jsonify, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'staticfiles/uploads'
+app.config['BATCH_SIZE'] = 3
 
 # Ensure the uploads directory exists
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
+# Global variables to hold the current batch of data and metadata
+current_batch = []
+batch_index = 0  # Keep track of the batch index
+total_rows = 0  # Total number of rows in the CSV
 
-# @app.route('/')
-# def index():
-#     return render_template('upload.html')
 
-
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     file = request.files['file']
-#     if file and file.filename.endswith('.csv'):
-#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-#         return redirect(url_for('display_api', filename=file.filename))
-#     else:
-#         return "Please upload a valid CSV file."
-
-@app.route('/file/api-data', methods=['POST', 'GET'])
-def display_api():
-    api_data = []
+def is_csv_empty():
     with open('staticfiles/uploads/homes.csv', 'r') as csvfile:
-        # next(csv)
-        csv_reader = csv.DictReader(csvfile)
-        for row in csv_reader:
-            api_data.append(dict(row))
+        csv_reader = csv.reader(csvfile)
+        return not any(csv_reader)  # Check if there are any rows
 
-    # Pass the data to the template
-    return jsonify(api_data)
+
+# Load the CSV data initially
+def load_csv_data():
+    global current_batch, batch_index, total_rows
+
+    if is_csv_empty():
+        return  # If CSV is empty, don't load any data
+
+    with open('staticfiles/uploads/homes.csv', 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        rows = list(csv_reader)
+        total_rows = len(rows)
+
+        start_idx = batch_index * app.config['BATCH_SIZE']
+        end_idx = start_idx + app.config['BATCH_SIZE']
+        current_batch = [dict(row) for row in rows[start_idx:end_idx]]
+        batch_index += 1
+
+        # Check if all data has been served, if so, stop the scheduler
+        if start_idx >= total_rows:
+            try:
+                scheduler.shutdown()
+            except RuntimeError:
+                pass  # Ignore the error if the scheduler is already shut down
+
+
+load_csv_data()
+
+
+def reload_data():
+    try:
+        load_csv_data()
+    except RuntimeError:
+        pass
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=reload_data, trigger='interval', seconds=5)
+scheduler.start()
+
+
+@app.route('/api/data', methods=['GET'])
+def render_html():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['GET'])
+def get_data():
+    global current_batch, total_rows
+
+    if len(current_batch) == 0:
+        if batch_index * app.config['BATCH_SIZE'] >= total_rows:
+            return jsonify(message='All data has been served.')
+
+    # Serialize the current_batch to JSON with indentation for pretty printing
+    pretty_json_data = json.dumps(current_batch, indent=4)
+
+    # Return the pretty-printed JSON response
+    response = app.response_class(
+        response=pretty_json_data,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 if __name__ == '__main__':
